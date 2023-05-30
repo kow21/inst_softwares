c0b2383541b69ea9dcb819fad88d8daf95967683
kow21 <kow21@users.noreply.github.com>
2023-05-30 22:57:45 +0900

    jsonのkeyからデータを表示・インストール作業を行えるように変更

--- inst_list.py
+++ inst_list.py
@@ -11,12 +11,12 @@ class MyApp(tk.Tk):
     json_datas = json.load(json_open)
 
 
-    
     def __init__(self):
         super().__init__()
         self.title("My App")
         self.geometry("300x200")
         self.create_widgets()
+
 
     def create_widgets(self):
         # リストボックスの作成
@@ -25,38 +25,31 @@ class MyApp(tk.Tk):
 
         # リストボックスに要素を追加
 
-        self.listBox.insert(tk.END, "Raspberry Pi Imager")
-        self.listBox.insert(tk.END, "Item 2")
-        self.listBox.insert(tk.END, "Item 3")
+        for inst_name in self.json_datas.keys():
+            self.listBox.insert(tk.END, inst_name)
+
 
         # リストボックスの要素をクリックしたら、関数を呼び出す
         self.listBox.bind("<<ListboxSelect>>", self.on_select)
 
+
     def on_select(self, event):
         # 選択された要素のインデックスを取得
         index = self.listBox.curselection()[0]
+        select_item = self.listBox.get(index)
 
         # 選択された要素に応じたスクリプトを実行
-        # self.install_data()
-        if index == 0:
-            # subprocess.call(["python", "./inst_scripts/script1.py", "rpi_imager"])
-            self.install_data("rpi_imager")
-        elif index == 1:
-            subprocess.call(["python", "./inst_scripts/script2.py"])
-        elif index == 2:
-            subprocess.call(["python", "./inst_scripts/script3.py"])
+        self.install_data(select_item)
             
+
     def install_data(self, inst_soft):
 
         # URL取得
         URL = self.json_datas[inst_soft]
 
-
         # ファイルパス取得
         dl_name = URL.split("/")[-1]
         current_path = Path().cwd()
-        # print(current_path.resolve())
-        # print(dl_name)
 
         # ファイルのDL、実行
         if not Path(current_path.joinpath(dl_name)).exists():
